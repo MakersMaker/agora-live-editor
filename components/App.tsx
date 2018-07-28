e2203a9302a01e3b758b8dd5096d64a44c0c1400
Alan Shum <alan200994@gmail.com>
2018-07-28 14:24:21 -0700

    init dashboard frontend oriented

--- components/App.tsx
+++ components/App.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import TradingHistory from './TradingHistory';
+import Dashboard from './Dashboard';
 
 export default class App extends React.Component {
   render() {
     return <>
-      <h1>EWQADA a react app</h1>
-      <TradingHistory />
+      <Dashboard />
     </>;
   }
 }

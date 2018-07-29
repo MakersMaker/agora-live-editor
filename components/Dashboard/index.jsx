e79204ff7154b3494fb804cc3dab7c97ef85287a
Alan Shum <alan200994@gmail.com>
2018-07-28 23:36:07 -0700

    agora integration

--- components/Dashboard/index.jsx
+++ components/Dashboard/index.jsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { extname } from 'path';
+import 'agora-signaling-sdk'; // exposed as window.Signal()
 import FileTree from '../FileTree';
 import Content from '../Content';
+import { Agora } from '../config';
 
 export default class Dashboard extends React.Component {
   state = {
     activeFileName: 'sample.json',
   }
 
-  switchFile(activeFileName) {
-    const ext = extname(activeFileName);
-    if (!ext) return;
-    this.setState({ activeFileName })
+  agora = {
+    signal: null
+  }
+
+  componentDidMount() {
+    this.registerAgoraClient();
   }
 
   render() {
@@ -22,5 +26,28 @@ export default class Dashboard extends React.Component {
         <Content activeFileName={ this.state.activeFileName } ></Content>
       </Grid>
     </>;
+  }
+
+  switchFile(activeFileName) {
+    const ext = extname(activeFileName);
+    if (!ext) return;
+    this.setState({ activeFileName })
+  }
+
+  registerAgoraClient() {
+    const agoraCred = Agora.generateToken(`${Math.round(Math.random() * 100)}@randomname.com`);
+    this.agora.signal = Signal(agoraCred.appId);
+    this.agora.session = this.agora.signal.login(agoraCred.account, agoraCred.token)
+    this.agora.session.onLoginSuccess = (uid) => {
+      console.log(uid);
+      // TODO save this to server as master if no record, or slave
+
+      this.agora.session.onMessageInstantReceive = function(account, uid, msg) {
+        console.log(msg);
+      }
+
+      // TODO try get master uid from server -> send to master uid
+      this.agora.session.messageInstantSend(uid, 'asds');
+    };
   }
 }

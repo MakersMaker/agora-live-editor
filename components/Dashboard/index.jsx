19bcd5037f945c12b5d9d6159ff7161802435484
Alan Shum <alan200994@gmail.com>
2018-07-28 18:20:41 -0700

    request remote file

--- components/Dashboard/index.jsx
+++ components/Dashboard/index.jsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Sidebar from '../Sidebar';
+import FileTree from '../FileTree';
 import Content from '../Content';
 
 export default class Dashboard extends React.Component {
   render() {
     return <>
       <Grid container>
-        <Sidebar></Sidebar>
+        <FileTree></FileTree>
         <Content></Content>
       </Grid>
     </>;
--- components/Editor/index.jsx
+++ components/Editor/index.jsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 import * as monaco from 'monaco-editor';
+
+const EditorWrapper = styled.div`
+  height: 100vh;
+  background-color: #000;
+`;
 
 export default class Editor extends React.Component {
   state = {
@@ -13,16 +19,23 @@ export default class Editor extends React.Component {
     this.editor = React.createRef();
   }
 
+  async switchFile(fileName) {
+    const rawResponse = await fetch('http://localhost:8082/files/sample.json');
+    const fileContent = await rawResponse.json();
+    console.log(fileContent);
+  }
+
   componentDidMount() {
     monaco.editor.create(this.editor.current, {
       value: this.state.code,
       language: this.state.language
     });
+    this.switchFile();
   }
 
   render() {
     return <Paper elevation={1}>
-      <div style={{ height: '100%' }} ref={this.editor}></div>
+      <div style={{ height: 'calc(100vh - 30px)' }} ref={this.editor}></div>
     </Paper>;
   }
 }

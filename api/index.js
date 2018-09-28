9a881d946bc61c6b73cc14f83fef9abe1d6ffd22
tmrl0419 <them0419@naver.com>
2018-09-28 17:20:08 +0900

    edit socket.io http to https

--- api/index.js
+++ api/index.js
@@ -30,24 +30,24 @@ var httpPort = 8082;
 var httpsPort = 8081;
 
 
-
-
-https.createServer(options,app).listen(httpsPort, (req,res) =>{
+const server = https.createServer(options,app).listen(httpsPort, (req,res) =>{
   console.log("Https server listening on port " + httpsPort);
 });
 
-const server = http.createServer(app).listen(httpPort, function(){  
+http.createServer(app).listen(httpPort, function(){  
   console.log("Http server listening on port " + httpPort);
 });
 
 const io = require('socket.io')(server);
+
 io.on('connection' , (socket)=>{
   socket.on('codeSend', (data)=>{
+    console.log("asdf");
     socket.broadcast.emit('codeRecive', data);
   });
-  // socket.on('messageSend' , (data)=>{
-  //   socket.broadcast.emit('messageRecive', data);
-  // });
+  socket.on('messageSend' , (data)=>{
+    socket.broadcast.emit('messageRecive', data);
+  });
 });
 
 app.get('/filestree', (req, res) => {
@@ -68,19 +68,6 @@ app.post('/files', (req, res) => {
 
 app.get('/lecturePage' , (req,res)=>{
   res.render('lecturePage.html');
-})
-
-app.get('/login', function (req, res){  
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h3>Login</h3>');
-  res.write('<form method="POST" action="/login">');
-  res.write('<label name="userId">UserId : </label>')
-  res.write('<input type="text" name="userId"><br/>');
-  res.write('<label name="password">Password : </label>')
-  res.write('<input type="password" name="password"><br/>');
-  res.write('<input type="submit" name="login" value="Login">');
-  res.write('</form>');
-  res.end();
 })
 
 
--- api/public/js/jsForLecturepage.js
+++ api/public/js/jsForLecturepage.js
@@ -67,89 +67,88 @@ window.onload = function(){
     document.getElementById("stop-recording").disabled = true;
     document.getElementById("download").disabled = true;
     var meeting = new Meeting();           
-            var meetingsList = document.getElementById('meetings-list');
-            var meetingRooms = {};
+    var meetingsList = document.getElementById('meetings-list');
+    var meetingRooms = {};
 
-            meeting.onmeeting = function (room) {
-                if (meetingRooms[room.roomid]) return;
-                meetingRooms[room.roomid] = room;
-                var tr = document.createElement('tr');
-                tr.innerHTML = '<li>' +'<td>'+ room.roomid + '</td>' +
-                    '<td><button class="join">Join</button></td></li>';
-                meetingsList.insertBefore(tr, meetingsList.firstChild);
-                console.log('asdflkajsdflk');
-                // when someone clicks table-row; joining the relevant meeting room
-                tr.onclick = function () {
-                    document.getElementById("hang-up").disabled = false;
-                    room = meetingRooms[room.roomid];
-                    // manually joining a meeting room
-                    if (room) meeting.meet(room);
-                    meetingsList.style.display = 'none';
-                    document.getElementById('hang-up').onclick= function(){
-                        meeting.leave();
-                        document.getElementById("setup-meeting").disabled = false;
-                    };
-                };
-
-                
-            };
-            var remoteMediaStreams = document.getElementById('remote-streams-container');
-            var localMediaStream = document.getElementById('local-streams-container');
-            // on getting media stream
-            meeting.onaddstream = function (e) {
-                if (e.type == 'local') localMediaStream.appendChild(e.video);
-                if (e.type == 'remote') remoteMediaStreams.insertBefore(e.video, remoteMediaStreams.firstChild);
-            };
-            
-            // via: https://github.com/muaz-khan/WebRTC-Experiment/tree/master/websocket-over-nodejs
-            meeting.openSignalingChannel = function(onmessage) {
-                var channel = location.href.replace(/\/|:|#|%|\.|\[|\]/g, '');
-                // wss://websocket-over-nodejs.herokuapp.com:443/
-                var websocket = new WebSocket('wss://webrtcweb.com:9449/');
-                websocket.onopen = function () {
-                    websocket.push(JSON.stringify({
-                        open: true,
-                        channel: channel
-                    }));
-                };
-                websocket.push = websocket.send;
-                websocket.send = function (data) {
-                    if(websocket.readyState != 1) {
-                        return setTimeout(function() {
-                            websocket.send(data);
-                        }, 300);
-                    }
-                    
-                    websocket.push(JSON.stringify({
-                        data: data,
-                        channel: channel
-                    }));
-                };
-                websocket.onmessage = function(e) {
-                    onmessage(JSON.parse(e.data));
-                };
-                return websocket;
-            };
-            // using firebase for signaling
-            // meeting.firebase = 'muazkh';
-            // if someone leaves; just remove his video
-            meeting.onuserleft = function (userid) {
+    meeting.onmeeting = function (room) {
+        if (meetingRooms[room.roomid]) return;
+        meetingRooms[room.roomid] = room;
+        var tr = document.createElement('tr');
+        tr.innerHTML = '<li>' +'<td>'+ room.roomid + '</td>' +
+            '<td><button class="join">Join</button></td></li>';
+        meetingsList.insertBefore(tr, meetingsList.firstChild);
+        console.log('asdflkajsdflk');
+        // when someone clicks table-row; joining the relevant meeting room
+        tr.onclick = function () {
+            document.getElementById("hang-up").disabled = false;
+            room = meetingRooms[room.roomid];
+            // manually joining a meeting room
+            if (room) meeting.meet(room);
+            meetingsList.style.display = 'none';
+            document.getElementById('hang-up').onclick= function(){
                 meeting.leave();
-                var video = document.getElementById(userid);
-                if (video) video.parentNode.removeChild(video);
+                document.getElementById("setup-meeting").disabled = false;
             };
-            // check pre-created meeting rooms
-            meeting.check();
-            document.getElementById('setup-meeting').onclick = function () {
-                // setup new meeting room
-                // console.log(editorTap);
-                document.getElementById("hang-up").disabled = false;
-                var meetingRoomName = document.getElementById('meeting-name').value || 'Simple Meeting';
-                meeting.setup(meetingRoomName);
-                this.disabled = true;
-                document.getElementById('hang-up').onclick= function(){
-                    meeting.leave();
-                    document.getElementById("setup-meeting").disabled = false;
-                };
-            };
+        };    
+    };
+            
+    var remoteMediaStreams = document.getElementById('remote-streams-container');
+    var localMediaStream = document.getElementById('local-streams-container');
+    // on getting media stream
+    meeting.onaddstream = function (e) {
+        if (e.type == 'local') localMediaStream.appendChild(e.video);
+        if (e.type == 'remote') remoteMediaStreams.insertBefore(e.video, remoteMediaStreams.firstChild);
+    };
+    
+    // via: https://github.com/muaz-khan/WebRTC-Experiment/tree/master/websocket-over-nodejs
+    meeting.openSignalingChannel = function(onmessage) {
+        var channel = location.href.replace(/\/|:|#|%|\.|\[|\]/g, '');
+        // wss://websocket-over-nodejs.herokuapp.com:443/
+        var websocket = new WebSocket('wss://webrtcweb.com:9449/');
+        websocket.onopen = function () {
+            websocket.push(JSON.stringify({
+                open: true,
+                channel: channel
+            }));
+        };
+        websocket.push = websocket.send;
+        websocket.send = function (data) {
+            if(websocket.readyState != 1) {
+                return setTimeout(function() {
+                    websocket.send(data);
+                }, 300);
+            }
+            
+            websocket.push(JSON.stringify({
+                data: data,
+                channel: channel
+            }));
+        };
+        websocket.onmessage = function(e) {
+            onmessage(JSON.parse(e.data));
+        };
+        return websocket;
+    };
+    // using firebase for signaling
+    // meeting.firebase = 'muazkh';
+    // if someone leaves; just remove his video
+    meeting.onuserleft = function (userid) {
+        meeting.leave();
+        var video = document.getElementById(userid);
+        if (video) video.parentNode.removeChild(video);
+    };
+    // check pre-created meeting rooms
+    meeting.check();
+    document.getElementById('setup-meeting').onclick = function () {
+        // setup new meeting room
+        // console.log(editorTap);
+        document.getElementById("hang-up").disabled = false;
+        var meetingRoomName = document.getElementById('meeting-name').value || 'Simple Meeting';
+        meeting.setup(meetingRoomName);
+        this.disabled = true;
+        document.getElementById('hang-up').onclick= function(){
+            meeting.leave();
+            document.getElementById("setup-meeting").disabled = false;
+        };
+    };
 }

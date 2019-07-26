14d55fcbca8c9bb5a3155849c5fdb6d729332aa7
KimDongGeun <ehdrms2034@naver.com>
2019-07-27 04:27:18 +0900

    ff

--- api/public/files/test.js
+++ api/public/files/test.js
@@ -3,33 +3,3 @@ console.log(a);
 for(let a=0; a<20; a++){
     console.log(a);
 }
-
-for(let a=0; a<30; a++){
-    console.log(a);
-}
-var a = ['a','b','c']
-console.log(a);
-for(let a=0; a<20; a++){
-    console.log(a);
-}
-
-for(let a=0; a<30; a++){
-    console.log(a);
-}var a = ['a','b','c']
-console.log(a);
-for(let a=0; a<20; a++){
-    console.log(a);
-}
-
-for(let a=0; a<30; a++){
-    console.log(a);
-}
-var a = ['a','b','c']
-console.log(a);
-for(let a=0; a<20; a++){
-    console.log(a);
-}
-
-for(let a=0; a<30; a++){
-    console.log(a);
-}
